830f24c12ba0649230c0aaead6ed5d29a4ab3e2b
Alejandra <aleexkj@gmail.com>
2016-04-28 18:59:14 -0500

    tuntun

---
+++ P02/file.py
@@ -0,0 +1,16 @@
+import sys
+
+def bytes_from_file(filename, chunksize=1):
+    with open(filename, "rb") as f:
+        chunk = f.read(chunksize)
+        while chunk:
+            for b in chunk:
+                yield b
+            chunk = f.read(chunksize)
+
+
+for b in bytes_from_file(sys.argv[1]):
+    fst = b & 15
+    snd = b >> 4
+    print(fst)
+    print(snd)
